e01ea94779505eea1c5463460f50f417250ca79c
Guillem <guillembaracco@hotmail.com>
2022-09-13 13:54:19 +0200

    can not sign in

--- routes/user.js
+++ routes/user.js
@@ -34,24 +34,51 @@ router.get('/me', isAuthenticated, async (req,res,next)=>{
 // @desc    Edit user from the Database
 // @route   PUT /user/edit
 // @access  Private
-router.put('/edit', isAuthenticated, fileUploader.single('userPicture'), async (req, res, next) => {
-    const userId = req.payload._id;
-    const { email, password, username, userPictureFilled } = req.body
-    let userPicture;
-    // if there is any issue, try to replace "files" to "file"
-    if (req.files) {
-      userPicture = req.files.path;
-    } 
-    else {
-      userPicture = userPictureFilled
+// router.put('/edit', isAuthenticated, fileUploader.single('userPicture'), async (req, res, next) => {
+//     const userId = req.payload._id;
+//     const { email, password, username, userPictureFilled } = req.body
+//     let userPicture;
+//     // if there is any issue, try to replace "files" to "file"
+//     if (req.files) {
+//       userPicture = req.files.path;
+//     } 
+//     else {
+//       userPicture = userPictureFilled
+//     }
+//     try {
+//       const userDataBase = await User.findByIdAndUpdate(userId, {email, password, username, userPicture, phoneNum }, {new: true});
+//       res.status(200).json({ data: userDataBase })
+//     } catch (error) {
+//       next(error);
+//     }
+//   });
+
+
+router.put('/edit', isAuthenticated, async (req, res, next) => {
+  const { email, username } = req.body;
+  // Check if email or password or name are provided as empty string 
+  if (email === "" || username === "") {
+    return next(new ErrorResponse('Please fill all the fields to register', 400))
+  }
+  // Use regex to validate the email format
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+  if (!emailRegex.test(email)) {
+    return next(new ErrorResponse('Email is not a valid format', 400))
+  }
+  try {
+    const user = await User.findById(req.payload._id);
+    if (!user) {
+      next(new ErrorResponse('No user found', 404));
+      return;
+    } else {
+      const updatedUser = await User.findByIdAndUpdate(req.payload._id, req.body, { new: true });
+      res.status(200).json({ data: updatedUser })
     }
-    try {
-      const userDataBase = await User.findByIdAndUpdate(userId, {email, password, username, userPicture}, {new: true});
-      res.status(200).json({ data: userDataBase })
-    } catch (error) {
-      next(error);
-    }
-  });
+  } catch (error) {
+    next(error);
+  }
+});
+
 
 // @desc    Delete a user
 // @route   DELETE /user/delete
