c6f9e8a1979d5c7efc8743dedca4cb051b618224
Guillem B <guillembaracco@gmail.com>
2022-09-15 08:44:05 +0200

    before to go to work

--- models/BetUser.js
+++ models/BetUser.js
@@ -4,32 +4,13 @@ const { Schema, model } = mongoose;
 const betUserSchema = new Schema ({
     userId: { type: Schema.Types.ObjectId, ref: "User" },
     dateLottery: { type: String, required: true },
-    numbers: {
-        type: [
-        {
-        num0: {
-         type: Number
-        },
-        num1: {
-         type: Number
-        },
-        num2: {
-         type: Number
-        },
-        num3: {
-         type: Number
-        },
-        num4: {
-         type: Number
-        },
-        num5: {
-         type: Number
-        }  
-    }
-    ],
-    required: true
-    },
-    numComplem: { type: Number,required: true },
+    num0: { type: Number, required: true, unique: true},
+    num1: { type: Number, required: true, unique: true},
+    num2: { type: Number, required: true, unique: true},
+    num3: { type: Number, required: true, unique: true},
+    num4: { type: Number, required: true, unique: true},
+    num5: { type: Number, required: true },
+    numComplem: { type: Number, required: true },
     numReint: { type: Number, required: true },
     euroBet: { type: Number, default: 1, required: true },
     isPrized: { type: Boolean, required: true }
