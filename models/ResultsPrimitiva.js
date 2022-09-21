9a33b5c938b1cb3ee7d932be51db611b38c3771c
Guillem B <guillembaracco@gmail.com>
2022-09-21 08:32:22 +0200

    saving changes in ResultsPrimitiva

--- models/ResultsPrimitiva.js
+++ models/ResultsPrimitiva.js
@@ -1,36 +1,35 @@
-const mongoose = require('mongoose');
-const { Schema, model } = mongoose;
+// const mongoose = require('mongoose');
+// const { Schema, model } = mongoose;
 
-const resultsPrimitivaSchema = new Schema ({
+// const resultsPrimitivaSchema = new Schema ({
     
-    numbers: {
-        reintegro : {type: Number},
-        numeros: [
+//     numbers: {
+//         reintegro : {type: Number},
+//         numeros: [
+//         ]
 
-        ]
+//     //     0: {
+//     //      type: Number
+//     //     },
+//     //     1: {
+//     //      type: Number
+//     //     },
+//     //     2: {
+//     //      type: Number
+//     //     },
+//     //     3: {
+//     //      type: Number
+//     //     },
+//     //     num4: {
+//     //      type: Number
+//     //     },
+//     //     num5: {
+//     //      type: Number
+//     //     }  
+//     // }
+//     // ],
+// {
+//   timestamps: true
+// });
 
-        0: {
-         type: Number
-        },
-        1: {
-         type: Number
-        },
-        2: {
-         type: Number
-        },
-        3: {
-         type: Number
-        },
-        num4: {
-         type: Number
-        },
-        num5: {
-         type: Number
-        }  
-    }
-    ],
-{
-  timestamps: true
-});
-
-module.exports = model("ResultPrimitiva", resultsPrimitivaSchema);
+// module.exports = model("ResultPrimitiva", resultsPrimitivaSchema);
